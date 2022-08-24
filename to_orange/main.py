a00eaf9e2a991385f0f197e1d104083c0eb1258e
Kuba-Wroblewski <j.rafal.wroblewski@gmail.com>
2022-08-24 21:46:46 +0200

    upgrade project orange

--- to_orange/main.py
+++ to_orange/main.py
@@ -16,24 +16,24 @@ class Request:
 
     def send_your_request(self):
         _response = requests.get(self._url)
-        _text = ('1. Sending a GET request to the specified host => ')
+        _text = "1. Sending a GET request to the specified host => "
         return str(_text)
 
     def response_time(self):
         _start = time.perf_counter()
         _status_code = requests.get(self._url)
         _stop = time.perf_counter()
-        _response = ('2. Time to receive a response from the host =>', round(_stop - _start, 4))
+        _response = "2. Time to receive a response from the host =>", round(_stop - _start, 4)
         return str(_response)
 
     def response_code(self):
         _status_code = requests.get(self._url)
-        _status = ('3. HTTP response code =>', _status_code, '2xx - Success Codes')
+        _status = "3. HTTP response code =>", _status_code, "2xx - Success Codes"
         return str(_status)
 
     def is_the_response_is_json(self):
         _validate = requests.get(self._url)
-        _validate = ('4. If the answer is: JSON =>', _validate.headers['content-type'])
+        _validate = "4. If the answer is: JSON =>", _validate.headers['content-type']
         return str(_validate)
 
     def json_validate(self):
@@ -43,21 +43,21 @@ class Request:
         except ValueError as err:
             print(err)
             return False
-        _response = '5. Validated if the JSON has the correct syntax =>', True, self.local_time()
+        _response = "5. Validated if the JSON has the correct syntax =>", True, self.local_time()
         return str(_response)
 
     def create_log_file(self):
         for _ in range(1):
             pass
             with open('log.txt', 'a') as f:
-                f.write(w.send_your_request()+w.response_time()+w.response_code()+w.is_the_response_is_json()
-                        + w.json_validate()+'\n')
-                print(w.send_your_request(), w.response_time(), w.response_code(), w.is_the_response_is_json(),
-                      w.json_validate())
+                f.write(run.send_your_request()+run.response_time()+run.response_code()+run.is_the_response_is_json()
+                        + run.json_validate()+'\n')
+                print(run.send_your_request(), run.response_time(), run.response_code(), run.is_the_response_is_json(),
+                      run.json_validate())
                 f.close()
 
 
-w = Request()
+run = Request()
 
 
 def time_loop():
