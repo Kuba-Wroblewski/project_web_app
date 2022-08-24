efbaa4f6b26de27549d35523579ba16e5101fba4
Kuba-Wroblewski <j.rafal.wroblewski@gmail.com>
2022-08-24 21:06:47 +0200

    upgrade project orange

--- to_orange/main.py
+++ to_orange/main.py
@@ -9,19 +9,16 @@ from threading import Timer
 class Request:
     _url = "http://api.nbp.pl/api/exchangerates/rates/a/eur/last/100/?format=json"
 
-    # local time
     def local_time(self):
         _seconds = time.time()
         _local_time = time.ctime(_seconds)
         return _local_time
 
-    # 1.
     def send_your_request(self):
         _response = requests.get(self._url)
         _text = ('1. Sending a GET request to the specified host => ')
         return str(_text)
 
-    # 2.
     def response_time(self):
         _start = time.perf_counter()
         _status_code = requests.get(self._url)
@@ -29,19 +26,16 @@ class Request:
         _response = ('2. Time to receive a response from the host =>', round(_stop - _start, 4))
         return str(_response)
 
-    # 3.
     def response_code(self):
         _status_code = requests.get(self._url)
         _status = ('3. HTTP response code =>', _status_code, '2xx - Success Codes')
         return str(_status)
 
-    # 4.
     def is_the_response_is_json(self):
         _validate = requests.get(self._url)
         _validate = ('4. If the answer is: JSON =>', _validate.headers['content-type'])
         return str(_validate)
 
-    # 5.
     def json_validate(self):
         _r = requests.get(self._url)
         try:
@@ -52,7 +46,6 @@ class Request:
         _response = '5. Validated if the JSON has the correct syntax =>', True, self.local_time()
         return str(_response)
 
-    # create file "log.txt"
     def create_log_file(self):
         for _ in range(1):
             pass
@@ -67,7 +60,6 @@ class Request:
 w = Request()
 
 
-# 6.
 def time_loop():
     Timer(1, time_loop).start()
     Request().create_log_file()
