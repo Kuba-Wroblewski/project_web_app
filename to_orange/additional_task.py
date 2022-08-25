5ac4bf20fe98e5c80f82ef8aecaf9c6157aa6d38
Kuba-Wroblewski <j.rafal.wroblewski@gmail.com>
2022-08-25 20:40:19 +0200

    upgrade project orange

--- to_orange/additional_task.py
+++ to_orange/additional_task.py
@@ -1,7 +1,30 @@
-
+""""[Zadanie dodatkowe] Program ma sprawdzić czy ceny EUR w 100 ostatnich notowaniach mieściły się
+    w zakresie od 4,5 do 4,7 zł, jeśli się nie mieściły to wykazać w jakich dniach."""
 import urllib.request
 import json
 import requests
 from urllib.request import urlopen
 
 url = "http://api.nbp.pl/api/exchangerates/rates/a/eur/last/100/?format=json"
+# with urllib.request.urlopen(url) as json_url:
+#     data = json.loads(json_url.read().decode())
+#     print(json.dumps(data, indent=4, sort_keys=True))
+
+
+with urlopen(url) as f:
+    data = f.read()
+    data = json.loads(data)
+    print(json.dumps(data, indent=2))
+
+    data = list(data)
+    print(data)
+    print(type(data))
+
+    for dict in data:
+        if 4.5 < dict[waluta] < 4.7:
+            print(dict[data])
+
+
+
+
+
--- to_orange/main.py
+++ to_orange/main.py
@@ -1,74 +1,67 @@
-#!/usr/bin/python3
+# !/usr/bin/python3
 
 import requests
 import time
 import json
-from threading import Timer
+from time import sleep
 
 
-class Request:
-    _url = "http://api.nbp.pl/api/exchangerates/rates/a/eur/last/100/?format=json"
+_url = "http://api.nbp.pl/api/exchangerates/rates/a/eur/last/100/?format=json"
 
-    def local_time(self):
-        _seconds = time.time()
-        _local_time = time.ctime(_seconds)
-        return _local_time
 
-    def send_your_request(self):
-        _response = requests.get(self._url)
-        _text = "1. Sending a GET request to the specified host => "
-        return str(_text)
+class RequestToApi:
+    def __init__(self, _url):
+        self._url = _url
 
-    def response_time(self):
-        _start = time.perf_counter()
-        _status_code = requests.get(self._url)
-        _stop = time.perf_counter()
-        _response = "2. Time to receive a response from the host =>", round(_stop - _start, 4)
-        return str(_response)
+    def _local_time(self):
+        seconds = time.time()
+        local_time = time.ctime(seconds)
+        return local_time
 
-    def response_code(self):
-        _status_code = requests.get(self._url)
-        _status = "3. HTTP response code =>", _status_code, "2xx - Success Codes"
-        return str(_status)
+    def _send_your_request(self):
+        response = requests.get(self._url)
+        text = "1. Sending a GET request to the specified host => "
+        return str(text)
+
+    def _response_time(self):
+        start = time.perf_counter()
+        status_code = requests.get(self._url)
+        stop = time.perf_counter()
+        response = "2. Time to receive a response from the host =>", round(stop - start, 4)
+        return str(response)
+
+    def _response_code(self):
+        status_code = requests.get(self._url)
+        status = "3. HTTP response code =>", status_code, "2xx - Success Codes"
+        return str(status)
 
     def is_the_response_is_json(self):
-        _validate = requests.get(self._url)
-        _validate = "4. If the answer is: JSON =>", _validate.headers['content-type']
-        return str(_validate)
+        validate = requests.get(self._url)
+        validate = "4. If the answer is: JSON =>", validate.headers['content-type']
+        return str(validate)
 
-    def json_validate(self):
-        _r = requests.get(self._url)
+    def _json_validate(self):
+        r = requests.get(self._url)
         try:
-            json.loads(_r.text)
+            json.loads(r.text)
         except ValueError as err:
             print(err)
             return False
-        _response = "5. Validated if the JSON has the correct syntax =>", True, self.local_time()
-        return str(_response)
+        response = "5. Validated if the JSON has the correct syntax =>", True, self._local_time()
+        return str(response)
 
     def create_log_file(self):
-        for _ in range(10):
-            pass
+        for i in range(10):
+            sleep(5)
             with open('log.txt', 'a') as f:
-                f.write(run.send_your_request()+run.response_time()+run.response_code()
-                        + run.is_the_response_is_json()+run.json_validate()+'\n')
+                f.write(self._send_your_request()+self._response_time()+self._response_code()
+                        + self.is_the_response_is_json()+self._json_validate()+'\n')
             file = open('log.txt', 'r')
             lines = file.readlines()
             print(lines[-1].rstrip('\n'))
             file.close()
 
 
-run = Request()
-
-
-def time_loop():
-    Timer(5, time_loop).start()
-    run.create_log_file()
-
-
-time_loop()
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    run = Request()
+    run = RequestToApi(_url)
+    run.create_log_file()
