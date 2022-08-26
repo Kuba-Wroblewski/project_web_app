9724503437bb27326df5ce62e7ab1a539312484a
Kuba-Wroblewski <j.rafal.wroblewski@gmail.com>
2022-08-26 06:16:42 +0200

    upgrade project orange

--- to_orange/additional_task.py
+++ to_orange/additional_task.py
@@ -1,38 +1,24 @@
-""""[Zadanie dodatkowe] Program ma sprawdzić czy ceny EUR w 100 ostatnich notowaniach mieściły się
-    w zakresie od 4,5 do 4,7 zł, jeśli się nie mieściły to wykazać w jakich dniach."""
-import urllib.request
+#!/usr/bin/python3
+
 import json
-import requests
 from urllib.request import urlopen
 
-url = "http://api.nbp.pl/api/exchangerates/rates/a/eur/last/100/?format=json"
-# with urllib.request.urlopen(url) as json_url:
-#     data = json.loads(json_url.read().decode())
-#     print(json.dumps(data, indent=4, sort_keys=True))
+_url = "http://api.nbp.pl/api/exchangerates/rates/a/eur/last/100/?format=json"
 
 
-with urlopen(url) as f:
-    data = f.read()
-    data = json.loads(data)
-    json.dumps(data, indent=2)
-
-    # data = list(data)
-    # print(data)
-    print(type(data))
-
-    # for dict in data:
-    #     print(data)
-    #     if 4.5 < dict['rates'] < 4.7:
-    #         print(dict['rates'])
-
-    for dict in data:
-        for item in data[dict]:
-            for key in item:
-                print(item)
-            # print(item)
-
-        # for dict in list:
-        #    if 4,5 < dict[waluta] < 4,7:
-        #       print(dict[data])
+def checkingUrl(self):
+    with urlopen(_url) as f:
+        data = f.read()
+        data = json.loads(data)
+        # json.dumps(data, indent=2)
+        for item in data['rates']:
+            date = item['effectiveDate']
+            price = item['mid']
+            if 4.7 < item['mid']:
+                print(date)
+            if 4.5 > item['mid']:
+                print(date)
 
 
+if __name__ == '__main__':
+    checkingUrl(_url)
